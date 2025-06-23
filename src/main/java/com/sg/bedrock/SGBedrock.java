10b8b8814567bd8ead7961909ba05f33f372d0a4
ArielM24 <ariel_montoya15@outlook.es>
2025-06-22 23:10:28 -0600

    1.21.6

--- src/main/java/com/sg/bedrock/SGBedrock.java
+++ src/main/java/com/sg/bedrock/SGBedrock.java
@@ -1,6 +1,16 @@
 package com.sg.bedrock;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerBlockEntityEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ChunkTicketType.Use;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,17 +18,25 @@ import org.slf4j.LoggerFactory;
 public class SGBedrock implements ModInitializer {
 	public static final String MOD_ID = "sg-bedrock";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		UseBlockCallback.EVENT.register((player, world, hand, result) -> {
+			ItemStack itemInHand = player.getStackInHand(hand);
+			String interactingBlockName = world.getBlockState(result.getBlockPos()).getBlock().getName().getString();
+			boolean isUsingDragonBreath = itemInHand.getItem().getName().getString()
+					.equals(Items.DRAGON_BREATH.getName().getString());
+			boolean isInteractingWithBedrock = interactingBlockName.equals(Blocks.BEDROCK.getName().getString());
+			if(!isInteractingWithBedrock || !isUsingDragonBreath){
+			return ActionResult.PASS;
+				
+			}
+			world.breakBlock(result.getBlockPos(), false);
+			player.increaseStat(Stats.USED.getOrCreateStat(itemInHand.getItem()), 1);
+			itemInHand.setCount(itemInHand.getCount() - 1);
+			player.giveItemStack(new ItemStack(Items.GLASS_BOTTLE));
+			return ActionResult.PASS;
+		});
 	}
 }
