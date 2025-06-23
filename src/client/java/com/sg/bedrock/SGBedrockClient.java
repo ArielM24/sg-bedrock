2debf083e5b788fce3f879820edf11991d4ca0d9
ArielM24 <ariel_montoya15@outlook.es>
2025-06-22 22:56:18 -0600

    init

---
+++ src/client/java/com/sg/bedrock/SGBedrockClient.java
@@ -0,0 +1,10 @@
+package com.sg.bedrock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SGBedrockClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
